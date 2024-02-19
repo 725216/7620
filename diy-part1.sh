eef4826b3ececa8d46da7f9fae887c21f1ee6ed2
725216 <djp.1@qq.com>
2024-02-19 19:06:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,10 @@
 #echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> "feeds.conf.default"
+#echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+#echo 'lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
+#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> "feeds.conf.default"
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt_passwart
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall_packages
+git clone https://github.com/sirpdboy/automount.git  package/automount
